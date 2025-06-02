6728019fff0d9cb261990a43bc695f8d2d745a6d
ahadnazar10 <ahad.adam08@gmail.com>
2025-06-02 11:54:29 +0530

    Done the project structure

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,14 @@
+export const signup = (req, res) => {
+  // Logic for user signup
+  res.send("Signup route");
+};
+
+export const login = (req, res) => {
+  // Logic for user login
+  res.send("Login route");
+};
+
+export const logout = (req, res) => {
+  // Logic for user logout
+  res.send("Logout route");
+};
